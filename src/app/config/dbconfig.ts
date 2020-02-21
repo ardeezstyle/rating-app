f0f9325ce700ec1760b1e26150c484314f2448db
Rupam Datta <ardeezstyle@gmail.com>
2020-02-21 18:26:07 +0530

    Adding interceptors for CORS request, HTTP PATCH request for update

--- src/app/config/dbconfig.ts
+++ src/app/config/dbconfig.ts
@@ -2,3 +2,5 @@ export const ADMINDB = 'https://rating-system-675db.firebaseio.com/administrator
 export const OWNERDB = 'https://rating-system-675db.firebaseio.com/owners.json';
 export const CUSTOMERDB = 'https://rating-system-675db.firebaseio.com/users.json';
 export const RATINGDB = 'https://rating-system-675db.firebaseio.com/ratings.json';
+
+export const OWNERPATH = 'https://rating-system-675db.firebaseio.com/owners';
