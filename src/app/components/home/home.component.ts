598325253448608d3e3d3b8bd2a16cc52382c13d
Rupam Datta <ardeezstyle@gmail.com>
2020-02-21 07:42:53 +0530

    Commented unnecessary codes in homepage

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -29,18 +29,18 @@ export class HomeComponent implements OnInit {
   }
 
 
-  fetchCenters() {
-    this.os.getAllCenters().subscribe(res => console.log('getAllCenters', res));
-    this.os.getOwner('-M-eGKEtpzW_bYnqFyGj').subscribe(res => console.log('getOwner', res));
-    this.os.getAllCentersByOwnerID('-M-eGKEtpzW_bYnqFyGj').subscribe(res => console.log('getAllCentersByOwnerID', res));
-
-    this.os.getCenterByKeyword('elhaven').subscribe(res => console.log('getCenterByKeyword', res));
-  }
-
-  fetchUsers() {
-    this.cs.getAllCustomersByOwnerID('-M-eGKEtpzW_bYnqFyGj').subscribe(res => console.log('getAllCustomersByOwnerID', res));
-    this.cs.getAllCustomersByCenterName('-M-eGKEtpzW_bYnqFyGj', 'Tom Keitons').subscribe(res => console.log('getAllCustomersByCenterName', res));
-  }
+  // fetchCenters() {
+  //   this.os.getAllCenters().subscribe(res => console.log('getAllCenters', res));
+  //   this.os.getOwner('-M-eGKEtpzW_bYnqFyGj').subscribe(res => console.log('getOwner', res));
+  //   this.os.getAllCentersByOwnerID('-M-eGKEtpzW_bYnqFyGj').subscribe(res => console.log('getAllCentersByOwnerID', res));
+  //
+  //   this.os.getCenterByKeyword('elhaven').subscribe(res => console.log('getCenterByKeyword', res));
+  // }
+  //
+  // fetchUsers() {
+  //   this.cs.getAllCustomersByOwnerID('-M-eGKEtpzW_bYnqFyGj').subscribe(res => console.log('getAllCustomersByOwnerID', res));
+  //   this.cs.getAllCustomersByCenterName('-M-eGKEtpzW_bYnqFyGj', 'Tom Keitons').subscribe(res => console.log('getAllCustomersByCenterName', res));
+  // }
 
   searchCenters(f: NgForm) {
     this.isloading = true;
